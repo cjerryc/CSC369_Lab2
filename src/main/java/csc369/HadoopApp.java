fc193ec4734c930132c1700e2a3b784e23d14b06
jerryc2 <jerryc2@illinois.edu>
2025-10-06 00:43:22 -0700

    Finished report 4

--- src/main/java/csc369/HadoopApp.java
+++ src/main/java/csc369/HadoopApp.java
@@ -92,7 +92,36 @@ public class HadoopApp {
             job.setReducerClass(BytesByClient.Reducer1.class);
             job.setOutputKeyClass(BytesByClient.OUTPUT_KEY_CLASS);
             job.setOutputValueClass(BytesByClient.OUTPUT_VALUE_CLASS);
-        } else {
+        }
+        // Report 4
+        else if ("RequestCountByClientAndURL".equalsIgnoreCase(otherArgs[0])) {
+            // ----- Job 1: Count requests per URL -----
+            job.setMapperClass(RequestCountByClientAndURL.Mapper1.class);
+            job.setReducerClass(RequestCountByClientAndURL.Reducer1.class);
+            job.setOutputKeyClass(RequestCountByClientAndURL.OUTPUT_KEY_CLASS_1);
+            job.setOutputValueClass(RequestCountByClientAndURL.OUTPUT_VALUE_CLASS_1);
+
+            // Set the input dir and output dir to print report to.
+            FileInputFormat.addInputPath(job, new Path(otherArgs[1]));
+            Path tempOutput = new Path("temp_requestcount_output");
+            FileOutputFormat.setOutputPath(job, tempOutput);
+
+            job.waitForCompletion(true);
+
+            // ----- Job 2: Sort by request count -----
+            Job job2 = new Job(conf, "Sort by Count");
+            job2.setMapperClass(RequestCountByClientAndURL.Mapper2.class);
+            job2.setReducerClass(RequestCountByClientAndURL.Reducer2.class);
+            job2.setOutputKeyClass(RequestCountByClientAndURL.OUTPUT_KEY_CLASS_2);
+            job2.setOutputValueClass(RequestCountByClientAndURL.OUTPUT_VALUE_CLASS_2);
+
+            // Set the input dir and output dir to print report to. Exit out of program after job completion.
+            FileInputFormat.addInputPath(job2, tempOutput);
+            FileOutputFormat.setOutputPath(job2, new Path(otherArgs[2]));
+
+            System.exit(job2.waitForCompletion(true) ? 0 : 1);
+        }
+        else {
             System.out.println("Unrecognized job: " + otherArgs[0]);
             System.exit(-1);
         }
--- src/main/java/csc369/RequestCountByClientAndURL.java
+++ src/main/java/csc369/RequestCountByClientAndURL.java
@@ -10,28 +10,31 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class RequestCountByHTTPCode {
+public class RequestCountByClientAndURL {
     // ----- Job 1 -----
     public static final Class OUTPUT_KEY_CLASS_1 = Text.class;
     public static final Class OUTPUT_VALUE_CLASS_1 = IntWritable.class;
-    //    Mapper 1 to Count Requests by HTTP code (Mapper is called once every line of the input text file)
+    //    Mapper 1 to Count Requests by Client for a Specific URl (Mapper is called once every line of the input text file)
     public static class Mapper1 extends Mapper<LongWritable, Text, Text, IntWritable> {
         private final static IntWritable one = new IntWritable(1);
-        private Text HTTPCode = new Text();
+        private Text Client = new Text();
 
         @Override
         protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
             // Using Apache log format: "IP - - [date] \"GET /path HTTP/1.1\" httpRes bytes"
             String line = value.toString();
             String[] parts = line.split(" ");  // convert the line to a string and get different fields from Apache log by space separator
-            if (parts.length > 8) {
-                String httpRes = parts[8];  // parts[8] is the HTTP RESPONSE CODE portion
-                HTTPCode.set(httpRes);
-                context.write(HTTPCode, one);  // set (key, value) pair as (URL, count)
+            if (parts.length > 6) {
+                String URL = parts[6];  // parts[6] is the URL portion
+                if (URL.equals("/twiki/bin/view/Main/WebHome")){
+                    String ip = parts[0];          // Client Hostname/IPv4 Address
+                    Client.set(ip);
+                    context.write(Client, one);  // set (key, value) pair as (URL, count)
+                }
             }
         }
     }
-    // Reducer 1 to Count Requests by HTTP Code (Hadoop groups the output from Mapper by unique keys, so when the reducer is applied
+    // Reducer 1 to Count Requests by Client for a Specific URl (Hadoop groups the output from Mapper by unique keys, so when the reducer is applied
     // to the data, the reduce method is called once on every unique key)
     public static class Reducer1 extends Reducer<Text, IntWritable, Text, IntWritable> {
         private IntWritable total = new IntWritable();
@@ -53,16 +56,16 @@ public class RequestCountByHTTPCode {
     //    Mapper 2 to sort by request count in ASCENDING order
     public static class Mapper2 extends Mapper<LongWritable, Text, IntWritable, Text> {
         private IntWritable count = new IntWritable();  // Hadoop variables to reuse for efficiency
-        private Text HTTPCode = new Text();
+        private Text Client = new Text();
 
         @Override
         protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
             // Input line: "/index.html    10"
             String[] parts = value.toString().split("\\s+");  //grab text input from input text file
             if (parts.length == 2) {
-                HTTPCode.set(parts[0]);
+                Client.set(parts[0]);
                 count.set(Integer.parseInt(parts[1]));
-                context.write(count, HTTPCode);  // set (key, value) pair as (count, HTTP response code)
+                context.write(count, Client);  // set (key, value) pair as (count, HTTP response code)
             }
         }
     }
